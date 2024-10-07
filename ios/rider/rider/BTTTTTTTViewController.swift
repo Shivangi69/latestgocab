ee2e2b074abde689e6792d5781c9c4f16dbff38d
Shivangi69 <97800318+Shivangi69@users.noreply.github.com>
2024-10-07 13:15:00 +0530

    done

---
+++ ios/rider/rider/BTTTTTTTViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BTTTTTTTViewController.swift
+//  rider
+//
+//  Created by Rohit wadhwa on 16/10/24.
+//  Copyright © 2024 minimal. All rights reserved.
+//
+
+import UIKit
+
+class BTTTTTTTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ios/rider/rider/Models/Objects/PrefferedAgencies.swift
+++ ios/rider/rider/Models/Objects/PrefferedAgencies.swift
@@ -20,74 +20,83 @@ struct AgencyData: Codable {
 }
 
 struct PreferredAgency: Codable {
-    let id: Int
+    let agencyId: Int
     let agencyName: String
-    let status: String
-    let approvalStatus: String
-    let registeredName: String
-    let logoUrl: String?
-    let registrationYear: String
-    let registrationAuthorityUrl: String?
-    let insuranceUrl: String?
-    let licenseUrl: String?
-    let address: String
-    let city: String?
-    let pincode: String?
-    let latitude: String?
-    let longtitude: String?
-    let fullname: String
-    let email: String
-    let phone: String
-    let lastUpdatedBy: String?
-    let isEmailVerified: Bool
-    let isPhoneVerified: Bool
-    let registeredAt: String?
-    let workingHours: String?
-    let totalDrivers: Int?
-    let totalVehicles: Int?
-    let affiliationsWithTaxiAssociation: String?
-    let password: String?
-    let accountNumber: String?
-    let bankName: String?
-    let bankRoutingNumber: String?
-    let bankSwift: String?
-    let createdAt: String
-    let updatedAt: String
+    let priority: Int = 0
+    
+    
+//    let id: Int
+//    let agencyName: String
+//    let status: String
+//    let approvalStatus: String
+//    let registeredName: String
+//    let logoUrl: String?
+//    let registrationYear: String
+//    let registrationAuthorityUrl: String?
+//    let insuranceUrl: String?
+//    let licenseUrl: String?
+//    let address: String
+//    let city: String?
+//    let pincode: String?
+//    let latitude: String?
+//    let longtitude: String?
+//    let fullname: String
+//    let email: String
+//    let phone: String
+//    let lastUpdatedBy: String?
+//    let isEmailVerified: Bool
+//    let isPhoneVerified: Bool
+//    let registeredAt: String?
+//    let workingHours: String?
+//    let totalDrivers: Int?
+//    let totalVehicles: Int?
+//    let affiliationsWithTaxiAssociation: String?
+//    let password: String?
+//    let accountNumber: String?
+//    let bankName: String?
+//    let bankRoutingNumber: String?
+//    let bankSwift: String?
+//    let createdAt: String
+//    let updatedAt: String
 
     // Custom coding keys to map snake_case in JSON to camelCase in Swift
     enum CodingKeys: String, CodingKey {
-        case id
-        case agencyName = "agency_name"
-        case status
-        case approvalStatus = "approval_status"
-        case registeredName = "registered_name"
-        case logoUrl = "logo_url"
-        case registrationYear = "registration_year"
-        case registrationAuthorityUrl = "registration_authority_url"
-        case insuranceUrl = "insurance_url"
-        case licenseUrl = "license_url"
-        case address
-        case city
-        case pincode
-        case latitude
-        case longtitude
-        case fullname
-        case email
-        case phone
-        case lastUpdatedBy = "last_updated_by"
-        case isEmailVerified = "isEmailVerified"
-        case isPhoneVerified = "isPhoneVerified"
-        case registeredAt = "registered_at"
-        case workingHours = "working_hours"
-        case totalDrivers = "total_drivers"
-        case totalVehicles = "total_vehicles"
-        case affiliationsWithTaxiAssociation = "affiliations_with_taxi_association"
-        case password
-        case accountNumber
-        case bankName
-        case bankRoutingNumber
-        case bankSwift
-        case createdAt = "created_at"
-        case updatedAt = "updated_at"
+        
+        case agencyId
+        case agencyName = "agencyName"
+        case priority
+//        case id
+//        case agencyName = "agency_name"
+//        case status
+//        case approvalStatus = "approval_status"
+//        case registeredName = "registered_name"
+//        case logoUrl = "logo_url"
+//        case registrationYear = "registration_year"
+//        case registrationAuthorityUrl = "registration_authority_url"
+//        case insuranceUrl = "insurance_url"
+//        case licenseUrl = "license_url"
+//        case address
+//        case city
+//        case pincode
+//        case latitude
+//        case longtitude
+//        case fullname
+//        case email
+//        case phone
+//        case lastUpdatedBy = "last_updated_by"
+//        case isEmailVerified = "isEmailVerified"
+//        case isPhoneVerified = "isPhoneVerified"
+//        case registeredAt = "registered_at"
+//        case workingHours = "working_hours"
+//        case totalDrivers = "total_drivers"
+//        case totalVehicles = "total_vehicles"
+//        case affiliationsWithTaxiAssociation = "affiliations_with_taxi_association"
+//        case password
+//        case accountNumber
+//        case bankName
+//        case bankRoutingNumber
+//        case bankSwift
+//        case createdAt = "created_at"
+//        case updatedAt = "updated_at"
     }
 }
